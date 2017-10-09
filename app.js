5847ad85210ecbf5d83081c3b3ffa10c67b3c603
lizncq <lizxd10@gmail.com>
2017-10-09 18:12:15 -0500

    archivosDelProyecto

---
+++ app.js
@@ -0,0 +1,20 @@
+//creando funcion cipher
+function cipher (sentence){
+  array = [];
+  sentence = prompt('ingrese una frase');
+  
+  for(var i = 65; i <= 90; i++ ){
+    (i-65 + 33) % 26 + 65;
+  }
+
+
+}
+
+//creando funcion decipher
+function decipher (){
+
+}
+
+//(65-65 + 33) % 26 + 65
+//(a - 33) % 26
+//String.fromCharCode(7)
